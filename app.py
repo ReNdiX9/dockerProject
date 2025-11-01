44376b7b56b83d3337fd13f0b47746a5c2396624
ReNdiX9 <egor.rudiakov@gmail.com>
2025-11-01 15:06:11 -0600

    Added index.html and app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from data_analysis import generate_charts_for_web
+from pathlib import Path
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+if __name__ == "__main__": 
+    app.run(debug=True)
